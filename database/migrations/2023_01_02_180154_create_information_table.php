620489d331d18941f5b878c1ae4ae9dcbcecbdc6
Aleksandr Vasiljev <zaswet@gmail.com>
2023-01-02 20:05:39 +0200

    information migration controller model

---
+++ database/migrations/2023_01_02_180154_create_information_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('information', function (Blueprint $table) {
+            $table->id();
+            $table->text('about');
+            $table->text('refund_policy');
+            $table->text('terms_and_conditions');
+            $table->text('privacy_policy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('information');
+    }
+};
